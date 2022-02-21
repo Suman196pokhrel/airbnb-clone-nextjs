9dea8a206fb7ea8ffc1b972eb222003947108110
Suman <spokhrel196@gmail.com>
2022-02-21 16:48:28 +0530

    search results api phase complete

--- components/Header.js
+++ components/Header.js
@@ -17,7 +17,7 @@ import { DateRangePicker } from 'react-date-range';
 
 
 
-function Header() {
+function Header({placeholder}) {
 
      const [searchInput, setSearchInput] = useState('')
      const [startDate, setStartDate] = useState(new Date())
@@ -77,7 +77,7 @@ function Header() {
 
          {/* Middle  */}
          <div className='flex items-center md:border-2 rounded-full py-2 md:shadow-sm'>
-              <input value={searchInput} onChange={(e)=> setSearchInput(e.target.value)} className='pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400' type="text" placeholder='Start your search' />
+              <input value={searchInput} onChange={(e)=> setSearchInput(e.target.value)} className='pl-5 bg-transparent outline-none flex-grow text-sm text-gray-600 placeholder-gray-400' type="text" placeholder={placeholder? placeholder:'Start your search'} />
               <SearchIcon className='hidden md:inline-flex h-8 bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2'/>
               
          </div>
@@ -125,4 +125,6 @@ function Header() {
   )
 }
 
-export default Header
+export default Header;
+
+
